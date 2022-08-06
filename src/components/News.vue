e92de27ed9212271232f3836d15bee900fa5c03a
Natalya Yasinskaya <Natalya.ponomarenko.94@mail.ru>
2022-08-06 10:38:40 +0300

    add intersaction observer

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,62 +1,43 @@
 <template>
-  <ul>
-    <li v-for="item in news" :key="item.id">
-      <p>{{ item.title }}</p>
-      <p>{{ item.full_text }}</p>
-      <p>Rating: {{ item.rating }}</p>
-      <button v-on:click="() => deleteItem(item.id)">X</button>
-    </li>
-  </ul>
+    <ul>
+      <li v-for="item in news" :key="item.id">
+        <p>{{ item.title }}</p>
+        <p>{{ item.full_text }}</p>
+        <p>Rating: {{ item.rating }}</p>
+        <button v-on:click="() => deleteItem(item.id)">X</button>
+      </li>
+    </ul>
+    <Observer @intersect="intersected"/>
 </template>
 
 <script>
-import Paginate from "vuejs-paginate-next";
-import axios from 'axios';
+import Observer from "./Observer.vue";
 
 const BASE_URL = 'http://127.0.0.1:8000';
 
 export default {
   name: "MyTest",
   components: {
-    Paginate,
+    Observer,
   },
   data() {
-    return {
-      news: [
-        // {
-        //   id: 1,
-        //   title: 'Google title',
-        //   description: 'Google description',
-        //   link: 'www.google.com',
-        //   rating: 1,
-        // },
-        // {
-        //   id: 2,
-        //   title: 'Yandex title',
-        //   description: 'Yandex description',
-        //   link: 'www.yandex.com',
-        //   rating: 2,
-        // },
-        // {
-        //   id: 3,
-        //   title: 'Yahoo title',
-        //   description: 'Yahoo description',
-        //   link: 'www.yahoo.com',
-        //   rating: 3,
-        // }
-      ]
-    }
+   return {
+     news: [],
+     page: 1,
+     limit: 5,
+   }
   },
   methods: {
     deleteItem: function(id) {
       const deleteIndex = this.news.findIndex((item) => item.id === id);
       this.news.splice(deleteIndex, 1);
     },
+    async intersected() {
+      const res = await fetch(`${BASE_URL}/abc?page=${this.page}&limit=${this.limit}`);
+      const { data } = await res.json();
+      this.page++;
+      this.news = [...this.news, ...data];
+    },
   },
-  mounted() {
-    axios
-      .get(`${BASE_URL}/abc`)
-      .then(response => this.news = response.data.data);
-  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import Paginate from "vuejs-paginate-next";
 import router from "./router";
 
 import "./assets/main.css";
 
-const app = createApp(App).use(Paginate);
+const app = createApp(App);
 
 app.use(router);
 
