57b765fd2a07971ed0cfc6f875cc56ecd8fc2be7
Natalya Yasinskaya <Natalya.ponomarenko.94@mail.ru>
2022-08-07 22:42:08 +0300

    add styles

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,12 +1,12 @@
 <template>
-    <ul>
-      <li v-for="item in news" :key="item.id">
+    <ul class="news_list">
+      <li class="news_item" v-for="item in news" :key="item.id">
         <RouterLink :to="{ name: 'article', params: { id: item.id }}">
           <p>{{ item.title }}</p>
         </RouterLink>
         <p>{{ item.full_text }}</p>
         <p>Rating: {{ item.rating }}</p>
-        <button v-on:click="() => deleteItem(item.id)">X</button>
+        <button class="news_delete-btn" v-on:click="() => deleteItem(item.id)">Delete</button>
       </li>
     </ul>
     <Observer @intersect="intersected"/>
@@ -32,24 +32,26 @@ export default {
   },
   methods: {
     deleteItem: async function(id) {
-      const res = await fetch(`${BASE_URL}/delete-news`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: 'same-origin',
-        mode: 'cors',
-        body: JSON.stringify({ id }),
-      }
-    );
-      const { data } = await res.json();
-      console.log(data);
-      if (data.data === 'ok') {
+      try {
+        await fetch(`${BASE_URL}/delete-news`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: 'same-origin',
+          mode: 'cors',
+          body: JSON.stringify({ id }),
+        }
+      );
+      } catch (error) {
+        console.error(error);
+      } finally {
         const deleteIndex = this.news.findIndex((item) => item.id === id);
         this.news.splice(deleteIndex, 1);
       }
     },
     async intersected() {
+      console.log('test')
       const res = await fetch(`${BASE_URL}/abc?page=${this.page}&limit=${this.limit}`);
       const { data } = await res.json();
       this.page++;
