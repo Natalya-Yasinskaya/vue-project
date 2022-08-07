c57d3748c6cba046c64f6324e8bc308610559a34
Natalya Yasinskaya <Natalya.ponomarenko.94@mail.ru>
2022-08-07 22:55:23 +0300

    fix fetch

--- src/components/News.vue
+++ src/components/News.vue
@@ -19,7 +19,7 @@ import { RouterLink } from "vue-router";
 const BASE_URL = 'http://127.0.0.1:8000';
 
 export default {
-  name: "MyTest",
+  name: "News",
   components: {
     Observer,
   },
@@ -33,25 +33,22 @@ export default {
   methods: {
     deleteItem: async function(id) {
       try {
-        await fetch(`${BASE_URL}/delete-news`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          credentials: 'same-origin',
-          mode: 'cors',
-          body: JSON.stringify({ id }),
+        const formData = new FormData();
+        formData.append('id', id);
+        const data = await fetch(`${BASE_URL}/delete-news`, {
+            method: 'POST',
+            body: formData,
+          }
+        );
+        if (data.ok) {
+          const deleteIndex = this.news.findIndex((item) => item.id === id);
+          this.news.splice(deleteIndex, 1);
         }
-      );
       } catch (error) {
         console.error(error);
-      } finally {
-        const deleteIndex = this.news.findIndex((item) => item.id === id);
-        this.news.splice(deleteIndex, 1);
       }
     },
     async intersected() {
-      console.log('test')
       const res = await fetch(`${BASE_URL}/abc?page=${this.page}&limit=${this.limit}`);
       const { data } = await res.json();
       this.page++;
